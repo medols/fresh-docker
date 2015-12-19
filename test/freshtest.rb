a51c908663d81a44420dc30b05dfabd5eabfd1a3
macbookpr02010 <macbookpro2010@gianduia.net>
2015-12-20 00:17:11 +0100

    changed to new proc api

--- test/freshtest.rb
+++ test/freshtest.rb
@@ -1,72 +1,28 @@
-fresh(
-	proc{
-		7.times{|i|
-coef = [1, 2, 1, 2, 1, 2, 3, 2, 3, 2, 1, 2, 1, 2, 1, 2, 3, 2, 3, 2]
-			val=[0]
-			gr2=[1,2,3]
-			ch2=[0,0]
-			ms2=[0,0,0]
-			mpi_gather ch2 , ms2 , gr2
-			val[0] = ms2[0]*coef[7] + ms2[1]*coef[8] + ms2[2]*coef[9]
-			puts val
-		}
-	},
-	proc{
-		7.times{|i|
-coef = [1, 2, 1, 2, 1, 2, 3, 2, 3, 2, 1, 2, 1, 2, 1, 2, 3, 2, 3, 2]
-			gr1=[4,5]
-			ch1=[0,0]
-			ms1=[0,0]
-			gr2=[0]
-			ms2=[0,10]
-			mpi_gather ch1 , ms1 , gr1
-			ms2[1]=ms1[0]*coef[1]+ms1[1]*coef[2]
-			mpi_bcast ms2 , gr2
-		}
-	},
-	proc{
-		7.times{|i|
-coef = [1, 2, 1, 2, 1, 2, 3, 2, 3, 2, 1, 2, 1, 2, 1, 2, 3, 2, 3, 2]
-			gr1=[4,5]
-			ch1=[0,0]
-			ms1=[0,0]
-			gr2=[0]
-			ms2=[1,11]
-			mpi_gather ch1 , ms1 , gr1
-			ms2[1]=ms1[0]*coef[3]+ms1[1]*coef[4]
-			mpi_bcast ms2 , gr2
-		}
-	},
-	proc{
-		7.times{|i|
-coef = [1, 2, 1, 2, 1, 2, 3, 2, 3, 2, 1, 2, 1, 2, 1, 2, 3, 2, 3, 2]
-			gr1=[4,5]
-			ch1=[0,0]
-			ms1=[0,0]
-			gr2=[0]
-			ms2=[2,12]
-			mpi_gather ch1 , ms1 , gr1
-			ms2[1]=ms1[0]*coef[5]+ms1[1]*coef[6]
-			mpi_bcast ms2 , gr2
-		}
-	},
-	proc{
-		7.times{|i|
-			gr1=[1,2,3]
-			ms1=[0,32]
-			val=[4,2,3,8,4,6,1]
-			ms1[1]=val[i]
-			mpi_bcast ms1 , gr1
-		}
-	},
-	proc{
-		7.times{|i|
-			gr1=[1,2,3]
-			ms1=[1,32]
-			val=[2,4,8,16,32,64,48]
-			ms1[1]=val[i]
-			mpi_bcast ms1 , gr1
-		}
-	}
-)
+puts (proc{ |rank,size|
+      7.times.map{|i|
+        coef=[1,2,1,2,1,2,3,2,3,2]
+        val0=[0]
+        val1=[4,2,3,8,4,6,1]
+        val2=[2,4,8,16,32,64,48]
 
+        sbuf1=[0]
+        rbuf1=[0,0]
+        root1=[4,5]
+        comm1=[1,2,3]
+
+        sbuf1[0]=val1[i] if rank==4
+        sbuf1[0]=val2[i] if rank==5
+        mpi_allgatherv sbuf1 , rbuf1 , root1 , comm1 , rank
+
+        sbuf2=[0]
+        rbuf2=[0,0,0]
+        root2=0
+        comm2=[1,2,3]
+
+        sbuf2[0]=rbuf1[0]*coef[2*rank-1]+rbuf1[1]*coef[2*rank] if comm2.include? rank
+        mpi_gatherv sbuf2 , rbuf2 , root2 , comm2 , rank
+
+        val0[0] = rbuf2[0]*coef[7] + rbuf2[1]*coef[8] + rbuf2[2]*coef[9]
+        val0
+      }
+    }*6).first.to_s
